f942df6c0c6441761f511a77d661bfd1f2e17e0c
Arisa Rae <Playarisa24@gmail.com>
2023-11-03 22:33:11 +0700

    transfer assets html css js

---
+++ script.js
@@ -0,0 +1,20 @@
+function submit() {
+    const email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const book = document.getElementById("book").value;
+    const message = document.getElementById("message").value;
+    if (email == "" || name == "" || book == "" || message == "") {
+        prompt("Please finish up the form before submitting.");
+    }
+    else {
+        prompt("Form submitted succesfully! Congratulation " + name + "!");
+    }
+}
+
+function openNav() {
+    document.getElementById("sideNav").style.width = "55%";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
