a391c285eff532f089e2291dc111d1d7cf7a9d60
Arisa Rae <Playarisa24@gmail.com>
2023-11-04 00:29:32 +0700

    update html css

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function submit() {
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
+    const birth = document.getElementById("birth").value;
     const book = document.getElementById("book").value;
     const message = document.getElementById("message").value;
-    if (email == "" || name == "" || book == "" || message == "") {
+    if (email == "" || name == "" || birth == "" || book == "" || message == "") {
         prompt("Please finish up the form before submitting.");
     }
     else {
