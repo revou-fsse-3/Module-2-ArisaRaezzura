f396d25b20bbd9ea5a02df1557ff3952939dfb96
Arisa Rae <Playarisa24@gmail.com>
2023-11-04 00:47:26 +0700

    update netlify link

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+|Project Name (Week)|Link                         |
+|-------------------|-----------------------------|
+|Bookworms (Week 1) |https://week1rae.netlify.app/|
+|Bookworms (Week 2) |https://week2rae.netlify.app/|
+|Bookworms (Week 3) |https://week3rae.netlify.app/|
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/DUj7T_Sj)
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function submit() {
+function submit(event) {
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
     const birth = document.getElementById("birth").value;
     const book = document.getElementById("book").value;
     const message = document.getElementById("message").value;
     if (email == "" || name == "" || birth == "" || book == "" || message == "") {
+        event.preventDevault()
         prompt("Please finish up the form before submitting.");
     }
     else {
+        event.preventDevault()
         prompt("Form submitted succesfully! Congratulation " + name + "!");
     }
 }
